4b2f48444eb65697449488401478cea439cfe957
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2022-07-14 14:47:33 +0200

    Aggiunta rotta any e istallato il vue-router

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('guest.welcome');
-})->name('home');
-
 Auth::routes();
 
 Route::middleware('auth')
@@ -28,3 +24,6 @@ Route::middleware('auth')
         Route::resource('posts', 'PostController');
     });
 
+Route::get('{any?}', function(){
+    return view('guest.welcome');
+})->where('any', '.*')->name('home');
